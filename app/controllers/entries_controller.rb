03d1dec8cb4511a3876936a9ef306fd752433e99
Alexis Pham <alexis.pham@kellogg.northwestern.edu>
2024-05-17 21:07:53 +0000

    c

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,10 @@
 class EntriesController < ApplicationController
-def show 
-  @title = Entry.find_by({"place_id" => @place["id"]}) 
-  @posted_on = Entry.find_by({"entry_id" => @entry["id"]}) 
-  @description = Entry.find_by({"entry_id" => @entry["id"]}) 
-end 
 
 def new 
   @entry = Entry.new 
   @entry["title"] = params["title"]
   @entry["description"] = params["description"]
+  @place = Place.find(@place["id"])
   @entry.save
   redirect_to "/places/#{@place["id"]}" 
 end 
