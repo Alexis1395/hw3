222060d6050ad65a1c9ac982ac956060858c2c1f
Alexis Pham <alexis.pham@kellogg.northwestern.edu>
2024-05-17 16:21:45 +0000

    c

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,13 @@
 class Entry < ApplicationRecord
   # talks to the "entries" table
+  def change 
+    create_table :entries do |t|
+      t.string "title"
+      t.text "description"
+      t.date "posted_on"
+      t.integer "place_id"
+
+      t.timestamps
+    end 
+  end 
 end
